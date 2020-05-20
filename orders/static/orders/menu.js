70dcc35aeb5ea093a892849268aac04a5134841e
Lukasz Chrzanowski <lukaszchrzanowski@me.com>
2020-05-20 12:31:49 +0200

    add tooltip for special items. refactor menu html/js: cleanup, csrf from cookie for ajax reqs

--- orders/static/orders/menu.js
+++ orders/static/orders/menu.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+	//static tippy instances(set inside html template)
+	//e.g. info toolptip for special menu items
+	tippy('[data-tippy-content]', {
+		placement: 'right',
+	});
+
+	//add to cart for items with addons
+	//1)init tippy with placeholder 
+	//2)get available addons via ajax and show form inside tippy
+	//3)add to cart item with selected addons when form submitted
 	tippy('.add-to-cart-addons-btn', {
 	  content: 'Loading...',
 	  trigger: 'click',
@@ -13,16 +23,94 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 
 	//add to cart for items without addons
+	//add to cart via ajax and show confrimation inside tippy
 	document.querySelectorAll('.add-to-cart-form')
 		.forEach(form => form.onsubmit = 
 			e => submit(e.target) 
 	);
 
+	//render DOM elements
 	renderCart();
-
 });
 
 
+//templates
+
+const add_to_cart_form_template = Handlebars.compile(`
+<form class="add-to-cart-form">
+<input type="hidden" name="item_id" value={{ item_id }}>
+{{#each addons}}
+	<label>{{ this.name }}{{ formatAddonPrice this.price }}</label>
+	<input type="checkbox" name="addon" value="{{ this.id }}">
+	<br>
+{{/each}}
+<input type=submit value="Add to Cart">
+</form>`);
+
+// const add_to_cart_form_template = Handlebars.compile(`
+// <form class="add-to-cart-form">
+// <input type="hidden" name="csrfmiddlewaretoken" value="{{ csrf_token }}">
+// <input type="hidden" name="item_id" value={{ item_id }}>
+// {{#each addons}}
+// 	<label>{{ this.name }}{{ formatAddonPrice this.price }}</label>
+// 	<input type="checkbox" name="addon" value="{{ this.id }}">
+// 	<br>
+// {{/each}}
+// <input type=submit value="Add to Cart">
+// </form>`);
+
+const cart_template = Handlebars.compile(`
+	{{#each order.order_items}}
+		{{this.menu_item.size.name}}
+		{{this.menu_item.category.name}}
+		</br>
+		{{this.menu_item.name}}
+		</br>
+		\${{this.menu_item.price}}
+		</br>
+		{{#each this.addons}}
+			+{{this.menu_item_addon.name}}
+			{{ formatAddonPrice this.menu_item_addon.price }}
+			<br/>
+		{{/each}}
+		<br/>
+	{{/each}}
+	<div id="total-price"><h3>Total: \${{ order.total_price }}</h3></div>`);
+
+//returns empty string if price==0.00 otherwise adds $ symbol in the front
+Handlebars.registerHelper('formatAddonPrice', price => price == 0.00 ? '' : `(+\$${price})`);
+
+
+
+async function renderCart() {
+	//fetch existing pending order
+	let response = await fetch(`/api/orders/?status=Pending&customer=1`)
+	if (!response.ok)
+		throw 'Failed to fetch pending order.';
+	const orders = await response.json();
+	if (orders.length == 0)
+		return; //nothing to render
+
+	//fetch order details
+	const order_id = orders[0].id;
+	response = await fetch(`/api/orders/${order_id}`);
+	if (!response.ok)
+		throw 'Failed to fetch order details.';
+	const order = await response.json();
+	const total_price = order.total_price;
+	//const order_items = order.order_items;
+
+	//render order 
+	//header
+	document.querySelector('#cart-header').innerHTML = `Cart (\$${total_price.toFixed(2)})`;
+	//body
+	const cart = cart_template({order: order})
+	const cart_body = document.querySelector('#cart-body');
+	cart_body.innerHTML = cart;
+	//enable checkout button if at least one item added to cart
+	if (order.order_items.length > 0)
+		document.querySelector('#checkout-btn').disabled = false;
+}
 
 async function renderAddToCart(instance) {
 	const item_id = instance.reference.dataset.item;
@@ -41,102 +129,21 @@ async function renderAddToCart(instance) {
 	instance.popper.childNodes[0].onsubmit =
 		e => {
 			const form_data = new FormData(e.target);
-			fetch('/addToCart', {method: 'post', body: form_data})
+			const headers = {'X-CSRFToken' : Cookies.get('csrftoken'),}
+			fetch('/addToCart', {method: 'post', body: form_data, headers: headers})
 			.then(response => response.json())
 			.then(data => instance.setContent(data.message))
 			renderCart();
 			return false; 
 		}
-
 }
-
-async function renderCart() {
-	//fetch existing pending order
-	let response = await fetch(`/api/orders/?status=Pending&customer=1`)
-	if (!response.ok)
-		throw 'Failed to fetch pending order.';
-	const orders = await response.json();
-	if (orders.length == 0)
-		return; //nothing to render
-
-	//fetch order details
-	const order_id = orders[0].id;
-	response = await fetch(`/api/orders/${order_id}`);
-	if (!response.ok)
-		throw 'Failed to fetch order details.';
-	const order = await response.json();
-	const total_price = order.total_price;
-	const order_items = order.order_items;
-
-	//render order 
-	document.querySelector('#cart-header').innerHTML = `Cart (\$${total_price})`;
-
-	const cart = cart_template({order: order})
-	const cart_body = document.querySelector('#cart-body');
-	cart_body.innerHTML = cart;
-	// console.log(order);
-
-
-	// order_items.forEach(order_item => {
-	// 	cart_body.innerHTML += order_item.menu_item.name;
-	// });
-
-	
-
-	
-
-}
-
-const add_to_cart_form_template = Handlebars.compile(`
-<form class="add-to-cart-form">
-<input type="hidden" name="csrfmiddlewaretoken" value="{{ csrf_token }}">
-<input type="hidden" name="item_id" value={{ item_id }}>
-{{#each addons}}
-	<label>{{ this.name }}{{ formatAddonPrice this.price }}</label>
-	<input type="checkbox" name="addon" value="{{ this.id }}">
-	<br>
-{{/each}}
-<input type=submit value="Add to Cart">
-</form>`);
-
-const cart_template = Handlebars.compile(`
-	{{#each order.order_items}}
-		{{this.menu_item.size.name}}
-		{{this.menu_item.category.name}}
-		</br>
-		{{this.menu_item.name}}
-		</br>
-		\${{this.menu_item.price}}
-		</br>
-		{{#each this.addons}}
-			+{{this.menu_item_addon.name}}
-			{{ formatAddonPrice this.menu_item_addon.price }}
-			<br/>
-		{{/each}}
-
-		<br/>
-		---
-		<br/>
-	{{/each}}
-	<div id="total-price"><h3>Total: {{ order.total_price }}</h3></div>`);
-
-//returns empty string if price==0.00 otherwise adds $ symbol in the front
-Handlebars.registerHelper('formatAddonPrice', price => price == 0.00 ? '' : `(+\$${price})`);
-
-
-
-
-
 
 function submit(form) {
-	console.log("submit called");
 	const form_data = new FormData(form);
 	fetch('/addToCart', {method: 'post', body: form_data})
 		.then(response => {
 			if (!response.ok) {
 				throw 'Failed to add to cart.';
-				console.log(response.status);
-				console.log(response);
 			}
 			response.json().then(data => {
 				const submit_btn = form.querySelector('input[type=submit]');
@@ -150,7 +157,6 @@ function submit(form) {
 				renderCart();
 			});
 		})
-		.catch(err => console.log(err));
-		
+		.catch(err => console.log(err));	
 	return false;
 }
